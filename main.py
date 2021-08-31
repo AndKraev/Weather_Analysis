81695266b08ea9a22e171eb971a40bddc659ddbc
Andrey <akrstp@gmail.com>
2021-08-31 13:17:32 +0300

    polish add docstring

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import argparse
 import sys
-from pathlib import Path
 
 from WeatherAnalysis import WeatherAnalysis
 
 
-def arg_parser(args):
+def arg_parser(args) -> argparse:
+    """Function to obtain parameters from console to run the code
+
+    :param args: args for testing
+    :return: parameters from console
+    :type: argparse.parse_args
+    """
     parser = argparse.ArgumentParser(
         prog="Incredible Hotel Weather Analyzer",
-        description="""Unzips csv files from the input
-                                                     folder, find cities with most
-                                                     hotels in each country and writes
-                                                     data to output folder""",
+        description="""Unzips csv files from the input folder, find cities with most 
+        hotels in each country and writes data to output folder""",
     )
 
     parser.add_argument("indir", type=str, help="a path to an input folder with hotels")
@@ -19,21 +22,21 @@ def arg_parser(args):
     parser.add_argument(
         "--outdir",
         type=str,
-        help="a path to an output folder with results "
-        '(by default creates "Output" folder in the input folder)',
+        help="a path to an output folder with results (by default creates 'Output' "
+        "folder in the input folder)",
     )
 
     parser.add_argument(
         "--threads",
         type=int,
-        default=100,
-        help="a maximum number of threads to be used (default=100)",
+        default=1000,
+        help="a maximum number of threads to be used (default=1000)",
     )
 
     parser.add_argument(
         "--hotels",
         type=int,
-        default=3,
+        default=100,
         help="a maximum number of hotels to be written (default=100)",
     )
 
